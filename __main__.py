12d48260ac333ed6f9ae50fdd9cacaf45e340bfd
Gerry.qi <Gerry.qi@outlook.com>
2023-05-12 18:03:36 +0800

    WIP

--- __main__.py
+++ __main__.py
@@ -6,10 +6,23 @@ import time
 # 定义默认参数
 DEFAULT_LEADER_ID = 0
 DEFAULT_LEADER_ADDRESS = 'localhost'
-DEFAULT_LEADER_PORT = 8000
+DEFAULT_LEADER_PORT = 8001
 DEFAULT_HEARTBEAT_INTERVAL = 5
 DEFAULT_TIMEOUT = 3
 NODES = set()
+
+def get_server_list(leader_host='localhost',leader_port=8001)->list:
+    # 创建套接字对象并连接到服务器
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((leader_host,leader_port))
+    # 发送请求消息到服务器
+    request_message = 'GET_SERVER_LIST'
+    client_socket.send(request_message.encode())
+    # 获取服务器响应
+    response_message = client_socket.recv(1024).decode()      
+    # 关闭套接字连接
+    #client_socket.close()       
+    return response_message
 
 class Node:
     def __init__(self, id, host, port):
@@ -23,14 +36,20 @@ class Node:
         self.election_timeout = None
         self.heartbeat_interval = DEFAULT_HEARTBEAT_INTERVAL
         self.last_heartbeat_time = time.time() 
-           
+    
     def run(self):
         # 启动两个线程：一个负责作为Server监听其它节点发送的消息；
         # 另一个负责作为Client定时向Leader发送心跳消息。
         server_thread = threading.Thread(target=self.run_server)
         server_thread.start()
-        client_thread = threading.Thread(target=self.run_client)
-        client_thread.start()
+        data = get_server_list()
+        # 解析响应消息
+        server_list = [server.split(':') for server in data.split(',')] 
+        for server in server_list:
+            #print(f"Host: {server[0]}, Port: {server[1]}")
+            print(server)
+            #client_thread = threading.Thread(target=self.run_client)
+            #client_thread.start()
 
     def register_node(self):
         self.nodes.add(Node(self.id, self.host, self.port))
@@ -51,11 +70,16 @@ class Node:
             # 处理传入的数据，并回复一个响应
             data = conn.recv(1024)
             # 处理响应数据并输出
-            print('Server Received:', data.decode())
-            message = input('Server:Enter message to send: ').encode()
-            if message == b'exit':
+            #print('Server Received:', data.decode())
+            #response_message = input('Server:Enter message to send: ').encode()
+
+            # 如果是获取服务器列表请求，则返回其他服务器列表
+            if data == b'GET_SERVER_LIST':
+                server_list = ['localhost:8001', 'localhost:8002', 'localhost:8003']
+                response_message = ','.join(server_list)
+            elif data == b'exit':
                 break
-            conn.sendall(message)        
+            conn.sendall(response_message)        
         # 关闭连接
         conn.close()
         server_socket.close()
